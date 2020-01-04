cfdb26641bd7d047606ea471a98f784836e8b72a
xaxis <xaxis@xaxiss-MBP.wavecable.com>
2020-01-04 15:42:37 -0800

    Added answer to question 3, most elegant way to reverse a string in JavaScript

---
+++ question3.js
@@ -0,0 +1,13 @@
+// 3. Write a function, in any language or pseudocode, that will take a string
+// as input and produce the reverse of that string. Bonus points if you can reverse
+// the string in-place, without requiring a new string or array.
+
+/**
+ * A very simple function that will reverse a string.
+ * @param str
+ */
+function reverse_string(str) {
+    return str.split("").reverse().join("");
+}
+
+reverse_string("this string is about to be reversed!");
